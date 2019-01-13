41340f0e0fa1704f39141d1adca9a332cef2c523
Vishakh <kmsree1990@gmail.com>
2019-01-13 11:12:13 +0530

    reading from excel

--- CuccumberFromExcel/src/main/java/ExcelReader/ExcelReader.java
+++ CuccumberFromExcel/src/main/java/ExcelReader/ExcelReader.java
@@ -2,6 +2,14 @@ package ExcelReader;
 
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelReader {
 	
@@ -14,7 +22,20 @@ public class ExcelReader {
 
 	ClassLoader classLoader = getClass().getClassLoader();
 	File file = new File(classLoader.getResource(fileName).getFile());
-
+	try {
+		FileInputStream excelFile = new FileInputStream(file);
+		Workbook workbook =new XSSFWorkbook(excelFile);
+		Sheet sheet = workbook.getSheet("test");
+		Row row = sheet.getRow(0);
+		Cell username=row.getCell(0);
+		String uname=username.getStringCellValue();
+		System.out.println(uname);
+		excelFile.close();
+		
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 	
   }
 }
