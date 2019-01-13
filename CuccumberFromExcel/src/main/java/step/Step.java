40b711b81a9b9c5ea3c2009f8401539571dc2e0c
Vishakh <kmsree1990@gmail.com>
2019-01-13 13:07:19 +0530

    Steps

--- CuccumberFromExcel/src/main/java/step/Step.java
+++ CuccumberFromExcel/src/main/java/step/Step.java
@@ -1,33 +1,48 @@
 package step;
 
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import cucumber.api.PendingException;
+import ExcelReader.ExcelReader;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 
 public class Step {
 	
 	WebDriver driver;
+	String sentence=null;
+	String word=null;
 	
-	@Given("^user is already on Login Page$")
+	
+	
+	@Given("^user is already on WSD page$")
 	public void user_already_on_page(){
 		System.setProperty("webdriver.chrome.driver", "C:/Users/vishakh/Downloads/Compressed/chromedriver.exe");
 		
  		driver=new ChromeDriver();
  		driver.get("http://vishakh.pythonanywhere.com/");
  	}
-	@Then("^user enters \"([^\"]*)\" and \"([^\"]*)\"$")
-	public void user_enters_and(String name, String password)  {
-	    // Write code here that turns the phrase above into concrete actions
-		System.out.println(name);
-		System.out.println(password);
+	@Then("^user enters \\\"([^\\\"]*)\\\"$")
+	public void user_enters_and(String dataid)  {
+		int row = Integer.parseInt(dataid);
+		ExcelReader excelreader=new ExcelReader();
+		excelreader.readSheet(row);
+		/*
+		 * System.out.println(sentence); System.out.println(word);
+		 */
 		driver.findElement(By.xpath("/html/body/form/div/div/div/p/table/tbody/tr[1]/td[4]/input")).clear();
-		driver.findElement(By.xpath("/html/body/form/div/div/div/p/table/tbody/tr[1]/td[4]/input")).sendKeys(name);
+		driver.findElement(By.xpath("/html/body/form/div/div/div/p/table/tbody/tr[1]/td[4]/input")).sendKeys(excelreader.sentence);
 		driver.findElement(By.xpath("/html/body/form/div/div/div/p/table/tbody/tr[2]/td[4]/input")).clear();
-		driver.findElement(By.xpath("/html/body/form/div/div/div/p/table/tbody/tr[2]/td[4]/input")).sendKeys(password);
-		driver.close();
+		driver.findElement(By.xpath("/html/body/form/div/div/div/p/table/tbody/tr[2]/td[4]/input")).sendKeys(excelreader.word);
+		
 		}
+	@And("^enters submit button$")
+	public void submit() throws InterruptedException {
+		driver.findElement(By.xpath("/html/body/form/div/div/div/p/table/tbody/tr[3]/td[4]/input")).click();
+		Thread.sleep(2500);
+		driver.close();
+	}
 }
